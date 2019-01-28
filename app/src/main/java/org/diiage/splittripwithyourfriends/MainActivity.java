d9dfa8d324fc44d0f9a06af45c6fc783ef606928
Unknown <matthieu.vuyge@gmail.com>
2019-01-28 13:46:44 +0100

    Add "create trip view"

--- app/src/main/java/org/diiage/splittripwithyourfriends/MainActivity.java
+++ app/src/main/java/org/diiage/splittripwithyourfriends/MainActivity.java
@@ -1,5 +1,6 @@
 package org.diiage.splittripwithyourfriends;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,12 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+
+        fab.setOnClickListener(view -> {
+            Intent i = new Intent(this, CreateTripActivity.class);
+            startActivity(i);
         });
     }
 
--- app/src/main/java/org/diiage/splittripwithyourfriends/MainActivityFragment.java
+++ app/src/main/java/org/diiage/splittripwithyourfriends/MainActivityFragment.java
@@ -1,10 +1,13 @@
 package org.diiage.splittripwithyourfriends;
 
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.diiage.splittripwithyourfriends.databinding.FragmentMainBinding;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        FragmentMainBinding binding = DataBindingUtil.setContentView(this.getActivity(), R.layout.fragment_main);
+        binding.setStrHelloWorld("Test binding");
+
+        return view;
     }
 }
