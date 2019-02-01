62f19aad57aef0a531f2bc62b2c7a3988f5a9e65
Unknown <matthieu.vuyge@gmail.com>
2019-02-01 14:00:50 +0100

    Finish add trip

--- app/src/main/java/org/diiage/splittripwithyourfriends/entities/Trip.java
+++ app/src/main/java/org/diiage/splittripwithyourfriends/entities/Trip.java
@@ -7,7 +7,12 @@ import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 @Entity(tableName = "Trips",
         foreignKeys = @ForeignKey(entity = Statut.class,
@@ -34,8 +39,15 @@ public class Trip {
     private long statutId;
 
     public Trip(String name, long statutId){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
         this.name= name;
         this.statutId = statutId;
+        this.beginDate = sdf.format(new Date());
+        c.add(Calendar.DATE, 1);
+        this.endDate = sdf.format(c.getTime());
     }
 
     public String getName() {
--- app/src/main/java/org/diiage/splittripwithyourfriends/ui/createtrip/CreateTripFragment.java
+++ app/src/main/java/org/diiage/splittripwithyourfriends/ui/createtrip/CreateTripFragment.java
@@ -1,6 +1,7 @@
 package org.diiage.splittripwithyourfriends.ui.createtrip;
 
 import android.app.DatePickerDialog;
+import android.databinding.DataBindingUtil;
 import android.support.v4.app.Fragment;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -12,11 +13,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.diiage.splittripwithyourfriends.R;
+import org.diiage.splittripwithyourfriends.databinding.CreateTripFragmentBinding;
 import org.diiage.splittripwithyourfriends.entities.Trip;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateTripFragment extends Fragment implements View.OnClickListener {
 
@@ -34,12 +40,21 @@ public class CreateTripFragment extends Fragment implements View.OnClickListener
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        //mViewModel = ViewModelProviders.of(this.getActivity()).get(CreateTripViewModel.class);
 
         View view = inflater.inflate(R.layout.create_trip_fragment, container, false);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+
         textViewTripName = view.findViewById(R.id.tripName);
         textViewBeginDate = view.findViewById(R.id.textViewbeginDate);
         textViewEndDate = view.findViewById(R.id.textViewEndDate);
+
+        textViewBeginDate.setText(sdf.format(new Date()));
+        c.add(Calendar.DATE, 1);
+        textViewEndDate.setText(sdf.format(c.getTime()));
 
         btnCreate = view.findViewById(R.id.buttonCreate);
         btnSetBeginDate = view.findViewById(R.id.btnStartDate);
@@ -57,19 +72,6 @@ public class CreateTripFragment extends Fragment implements View.OnClickListener
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(CreateTripViewModel.class);
         // TODO: Use the ViewModel
-    }
-
-    public void CreateTrip(View v){
-        TextView textView = v.findViewById(R.id.tripName);
-        TextView textViewBeginDate = v.findViewById(R.id.textViewbeginDate);
-        TextView textViewEndDate = v.findViewById(R.id.textViewEndDate);
-        String name = textView.getText().toString();
-        String beginDate = textViewBeginDate.getText().toString();
-        String endDate = textViewEndDate.getText().toString();
-        Trip t = new Trip(name, 1);
-        //TODO récupération du statut
-        mViewModel.insert(t);
-        //mViewModel.AddTrip(name);
     }
 
     @Override
@@ -95,6 +97,30 @@ public class CreateTripFragment extends Fragment implements View.OnClickListener
             datePickerDialog.show();
         }
         if(v == btnCreate) {
+            try {
+                String name = textViewTripName.getText().toString();
+                String beginDate = textViewBeginDate.getText().toString();
+                String endDate = textViewEndDate.getText().toString();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+                Date oBeginDate = sdf.parse(beginDate);
+                Date oEndDate = sdf.parse(endDate);
+
+                if(name.equals("")) {
+                    Toast.makeText(this.getContext(), R.string.empty_not_saved, Toast.LENGTH_LONG).show();
+                } else if(oBeginDate.after(oEndDate)) {
+                    Toast.makeText(this.getContext(), R.string.start_date_after_end_date, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Trip t = new Trip(name,1);
+                    t.setBeginDate(beginDate);
+                    t.setEndDate(endDate);
+                    mViewModel.insert(t);
+                    getActivity().finish();
+                }
+            } catch (Exception e) {
+                Toast.makeText(this.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
 
         }
     }
